b5e70b0fb3f8851375ac47dddfabd5ef63e0ab5a
SarthakNijhawan <sarthakn69@gmail.com>
2017-12-16 21:26:15 +0530

    mnist_ff_keras completed

--- tf_tut/mnist_ff_keras.py
+++ tf_tut/mnist_ff_keras.py
@@ -1,5 +1,7 @@
-import tensorflow as tf
+import cv2
 import numpy as np
+import tensorflow as tf
+
 from keras.layers import Dense, Dropout, Activation
 from keras.metrics import categorical_accuracy as accuracy
 from keras.objectives import categorical_crossentropy
@@ -8,7 +10,7 @@ from keras import backend as K
 from tensorflow.examples.tutorials.mnist import input_data
 mnist_data = input_data.read_data_sets('./tmp/MNIST_data', one_hot=True)
 
-# Global Variables
+#------------------------------------------ Global Variables
 n_epochs = 5
 
 sess = tf.Session()
@@ -18,6 +20,7 @@ K.set_session(sess)
 img = tf.placeholder(tf.float32, shape=(None, 784))
 labels = tf.placeholder(tf.float32, shape=(None, 10))
 
+#------------------------------------------ Architecture
 # Keras layers can be called on TensorFlow tensors:
 x = Dense(128, activation=None)(img)  # fully-connected layer with 128 units and ReLU activation
 # x = tf.nn.relu(x)
@@ -29,6 +32,8 @@ x = Activation('relu')(x)
 x = Dropout(0.5)(x)
 preds = Dense(10, activation='softmax')(x)  # output layer with 10 units and a softmax activation
 
+pred_class = tf.argmax(preds, axis=1)
+
 # loss funtion
 loss = tf.reduce_mean(categorical_crossentropy(labels, preds))
 
@@ -39,7 +44,7 @@ train_step = tf.train.GradientDescentOptimizer(0.5).minimize(loss)
 init_op = tf.global_variables_initializer()
 sess.run(init_op)
 
-# Run training loop for one epoch
+#------------------------------------------ Run training loop for one epoch
 with sess.as_default():
 	for ep in range(n_epochs):
 	    for i in range(100):
@@ -47,20 +52,41 @@ with sess.as_default():
 	        train_step.run(feed_dict={img: batch[0],
 	                                  labels: batch[1],
 	                                  K.learning_phase(): 1})
-	    print("Epoch : " + str(ep+1) +" loss is : " + loss.eval(sess, feed_dict={img: batch[0],
+
+	    print("Epoch : " + str(ep+1) + " loss is : " + str(loss.eval(feed_dict={img: batch[0],
 												                                 labels: batch[1],
-												                                 K.learning_phase(): 0}))
-# Accuracy metric
+												                                 K.learning_phase(): 0})))
+#------------------------------------------ Accuracy metric
 acc_value = tf.reduce_mean(accuracy(labels, preds))
 with sess.as_default():
     print(acc_value.eval(feed_dict={img: mnist_data.test.images,
                                     labels: mnist_data.test.labels,
                                     K.learning_phase(): 0}))
 
-# Saving the model
+#------------------------------------------ Saving the model
 saver = tf.train.Saver()
-saver.save(sess, './tmp/models/mnist_ff_keras', global_step=1)								# Let say global_step are the number of epochs the model has gone through
+saver.save(sess, './tmp/models/mnist_ff_keras/model_ff_keras')								# Let say global_step are the number of epochs the model has gone through
 
-# Load a model and predict
-# predictions = preds.eval(feed_dict={img: img_pred,
-# 									K.learning_phase(): 0})
+#------------------------------------------ Load a model and predict
+loaded_img = cv2.imread("img.jpg", 0)
+loaded_img = np.reshape(loaded_img, [1, 784])
+
+saver.restore(sess, './tmp/models/mnist_ff_keras/model_ff_keras')
+print(sess.run([preds, pred_class], feed_dict={img: loaded_img,
+									K.learning_phase(): 0}))
+
+#------------------------------------------ Aliter by loading the graph (.meta file) [Not working though :(, just decomment it ]
+# load meta graph and restore weights
+# saver = tf.train.import_meta_graph('./tmp/models/mnist_ff_keras/model_ff_keras.meta')
+# saver.restore(sess, tf.train.latest_checkpoint('./tmp/models/mnist_ff_keras/'))
+
+
+
+# graph = tf.get_default_graph()
+# # print(graph.get_operation_by_name("img:)"))
+# img = graph.get_tensor_by_name("img:0")														# Placeholder for prediction
+
+# # access the op
+# preds = graph.get_tensor_by_name("preds:0")
+# pred_class = graph.get_tensor_by_name("pred_class:0")
+
