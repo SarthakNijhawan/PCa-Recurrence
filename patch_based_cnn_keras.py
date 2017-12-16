500c19652625e27a4ddb34461a9d164073e30865
SarthakNijhawan <sarthakn69@gmail.com>
2017-12-17 00:04:12 +0530

    Completed some TODOs in modified file

--- patch_based_cnn_keras.py
+++ patch_based_cnn_keras.py
@@ -1,4 +1,4 @@
-import argparse														#TODO
+import argparse																						#TODO
 import numpy as np
 import os
 import pickle
@@ -15,99 +15,146 @@ from keras import backend as K
 
 """
 
-# Global Variables
-dropout_prob = 0.5													# Dropout layer probability
-n_epochs = 2														# Number of epochs cnn must be trained in each M step
-n_classes = 2														# Recurrent and Non-recurrent 
-batch_size = 128													# batch size for training the cnn
-percentile_1 = 0.4													# FIXME
-percentile_2 = 0.3													# FIXME
+# --------------------- Global Variables -------------------------
+model_path = './models/patch_based_cnn_model'
+patches_path = '../deepak/DB_HnE_101_anno_cent'
+images_path = '../deepak/sorted_patient_wise_normalized_dataset'
+
 n_iter = 1000														# number of iterations for EM algo to run
 
 
-# Default Session for Keras
-sess = tf.Session()
-K.set_session(sess)
+# --------------------- Main Function ----------------------------
+def main():
+	""" Trains the model on EM algo and saves it to the given model path """
 
-# -------------------- Helper functions ------------------------
-def load_image(img_path):
-	img = cv2.imread(img_path)
-	img_tensor = tf.convert_to_tensor(img)
-	return tf.reshape(img_tensor, [1,101,101,3])					# Reshaping so that it can be processed further into batches on axis=0
+	# Loads all the patches label wise dictionaries of image wise patches 
+	patches, initial_train_data = load_patches_image_wise(patches_path, images_path)				# TODO
 
-# local response normalization on images (Adapted Code)
-# def lrn(x):
-# 	""" Custom made normalisation layer for Keras """
-#     ones_for_weight = np.reshape(np.ones((32, 32)), (1,32,32))
-#     mu = sum_pool2d(x, pool_size = (7, 7), strides = (1, 1), padding = (3, 3))
-#     mu_weight = sum_pool2d(ones_for_weight, pool_size = (7, 7), strides = (1, 1), padding = (3, 3))
-#     sum_sq_x = sum_pool2d(K.square(x), pool_size = (7, 7), strides = (1, 1), padding = (3, 3))
-#     total_mu_sq = mu_weight * K.square(mu)
-#     sq_cross_term = -2 * K.square(mu)
-#     sigma = K.sqrt(sum_sq_x + total_mu_sq + sq_cross_term)
-#     return (x - mu)/(sigma + 1)
+	# Initial M-step
+	_, _, _, train_step = patch_based_cnn_model()
+	train(initial_train_data[0], initial_train_data[1], load=False)
 
-# # def lcn_output_shape(input_shape):
-# #     return input_shape
+	for itr in range(n_iter-1):
+		# E-step
+		predicted_maps = []
+		for i in range(2):
+			per_label_maps = {}
+			for image in patches[i].keys:
+				per_label_maps{image} = load_and_predict(patches{image})							# patches{image}.shape = [None, 101, 101, 3]
+			predicted_maps[i] = per_label_maps
+		train_data = E_step(predicted_maps)															# TODO
 
+		# M-Step
+		train(train_data[0], train_data[1], model_path)
+		# load_and_evaluate(test_data[0], test_data[1], model_path)									# TODO
+	
+	print("Completed!!")
 
-# def sum_pool2d(x, pool_size = (7, 7), strides = (1, 1), padding = (3, 3)):
-#     sum_x = pool.pool_2d(x, ds = pool_size, st = strides, mode = 'sum', padding = padding, ignore_border = True)
-#     return sum_x
+		
+##################################################################
+#---------------------Model and its functions -------------------#
+##################################################################
+def patch_based_cnn_model(dropout_prob=0.5, l_rate=0.5, n_classes=2):
 
-# # def sum_pool2d_output_shape(input_shape):
-# #     return input_shape
-
-def gkern(kernlen=101, nsig=3):
-    """Returns a 2D Gaussian kernel array."""
-
-    interval = (2*nsig+1.)/(kernlen)
-    x = np.linspace(-nsig-interval/2., nsig+interval/2., kernlen+1)
-    kern1d = np.diff(st.norm.cdf(x))
-    kernel_raw = np.sqrt(np.outer(kern1d, kern1d))
-    kernel = kernel_raw/kernel_raw.sum()
-    return kernel
-
-def cnn_model():
-	""" Model function for the CNN Model """
 	# Placeholders
 	img = tf.placeholder(tf.float32, shape=(None, 101, 101, 3))
 	labels = tf.placeholder(tf.float32, shape=(None, 2))
 
 	# Layers
-	conv1 = conv2D(80, 6, strides=1, padding='same', activation=None, kernel_initialiser='he_normal')(img)
+	conv1 = Conv2D(80, 6, strides=1, padding='same', activation=None, kernel_initializer='he_normal')(img)
 	conv1 = tf.nn.local_response_normalisation(conv1)										# FIXME
 	conv1 = Activation('relu')(conv1)
 	conv1 = MaxPooling2D(pool_size=(2, 2), strides=2)(conv1)
 
-	conv2 = conv2D(120, 5, strides=1, padding='same', activation=None, kernel_initialiser='he_normal')(conv1)
+	conv2 = Conv2D(120, 5, strides=1, padding='same', activation=None, kernel_initializer='he_normal')(conv1)
 	conv2 = tf.nn.local_response_normalisation(conv2)										# FIXME
 	conv2 = Activation('relu')(conv2)
 	conv2 = MaxPooling2D(pool_size=(2, 2), strides=2)(conv2)
 
-	conv3 = conv2D(160, 3, strides=1, padding='same', activation=None, kernel_initialiser='he_normal')(conv2)
+	conv3 = Conv2D(160, 3, strides=1, padding='same', activation=None, kernel_initializer='he_normal')(conv2)
 	
-	conv4 = conv2D(200, 3, strides=1, padding='same', activation=None, kernel_initialiser='he_normal')(conv3)
+	conv4 = Conv2D(200, 3, strides=1, padding='same', activation=None, kernel_initializer='he_normal')(conv3)
 	conv4 = MaxPooling2D(pool_size=(3, 3), strides=2)(conv4)
 
-	dense1 = Dense(320, activation='relu')(conv4)
+	conv4_flatten = tf.reshape(conv4, [-1, 9*9*200])
+	dense1 = Dense(320, activation='relu')(conv4_flatten)
 	dense1 = Dropout(dropout_prob)(dense1)
 
 	dense2 = Dense(320, activation='relu')(dense1)
 	dense2 = Dropout(dropout_prob)(dense2)
 
-	preds = Dense(2, activation='softmax')(dense2)
+	preds = Dense(n_classes, activation='softmax')(dense2)
+	pred_clas = tf.argmax(preds, axis=1)
 
 	# loss funtion
 	loss = tf.reduce_mean(categorical_crossentropy(labels, preds))
 
 	# Training operation
-	train_step = tf.train.GradientDescentOptimizer(0.5).minimize(loss)
-	
-	return [preds, loss, train_step]
+	train_step = tf.train.GradientDescentOptimizer(l_rate).minimize(loss)
 
-# -------------------- Functions for EM Algo -------------------
-def load_imagewise_patches():
+	# Accurace metric
+	acc_value = tf.reduce_mean(accuracy(labels, preds))
+
+	return [preds, pred_class, loss, train_step]
+
+def train(train_x, train_y, model_path, n_epochs=2, batch_size=128, load=True):
+	""" Loads the model from the given path, trains it for n_epochs 
+		and then saves the model back to the same 
+	"""
+
+	# Default Session for Keras
+	sess = tf.Session()
+	K.set_session(sess)
+
+	saver = tf.train.Saver()
+	if load:
+		saver.load(sess, model_path)
+
+	for epoch in xrange(1, n_epochs):
+		epoch_loss = 0
+		for i in range(train_x.shape[0]/batch_size):
+			sess.run(train_step, feed_dict={img: train_x[,:,:,:],										#TODO
+											labels: train_y[,:],										#TODO
+											K.learning_phase(): 1})
+		print("Epoch :", epoch, "loss is :", epoch_loss)
+
+	saver.save(sess, model_path)
+	sess.close()
+
+def load_and_predict(pred_x, model_path):
+	# Default Session for Keras
+	sess = tf.Session()
+	K.set_session(sess)
+
+	saver = tf.train.Saver()
+	saver.load(sess, model_path)
+
+	predictions = sess.run(preds, feed_dict={img: pred_x,
+											K.learning_phase(): 0})
+	close()
+	return predictions
+
+def load_and_evaluate(test_x, text_y, model_path):
+	# Default Session for Keras
+	sess = tf.Session()
+	K.set_session(sess)
+	
+	saver = tf.train.Saver()
+	saver.load(sess, model_path)
+
+	# Accuracy metric
+	with sess.as_default():
+	    accuracy = acc_value.eval(feed_dict={img: test_x,
+			                          		labels: text_y,
+			                           		K.learning_phase(): 0})
+	sess.close()
+	return accuracy
+
+
+##################################################################
+#--------------------- EM Algo Helper functions -------------------#
+##################################################################
+def load_patches_image_wise():
 	""" - Loads all the images and splits them into patches for prediction
 		and updation of training data for every iteration in the applied
 		EM algo for training he model
@@ -151,59 +198,6 @@ def load_imagewise_patches():
 
 	return img_wise_patches
 
-
-def prepare_train_data_from_img_wise_dict():
-	""" - Loads the initially extracted nucleus centric patches for training 
-		and treating all the patches as discrminiative initially
-		- Returns:
-
-	"""
-
-	pass
-
-def M_step():
-	""" - Trains the model with discrminative patches for n_epochs = 2 (here), 
-		and saves the model, useful in predicting the probability maps per image 
-		for the next E-step
-		- Returns:
-
-		"""
-	[output, loss, train_step] = cnn_model()
-
-	# Initialize all variables
-	init_op = tf.global_variables_initializer()
-	sess.run(init_op)
-	
-	# Run training loop
-	with sess.as_default():
-	    for i in range(100):
-	        batch = mnist_data.train.next_batch(50)
-	        train_step.run(feed_dict={img: batch[0],
-	                                  labels: batch[1],
-	                                  K.learning_phase(): 1})
-	# Accuracy metric
-	acc_value = accuracy(labels, preds)
-	with sess.as_default():
-	    print acc_value.eval(feed_dict={img: mnist_data.test.images,
-	                                    labels: mnist_data.test.labels,
-	                                    K.learning_phase(): 0})
-
-	# Prediction for a given batch of images
-	predictions = preds.eval(feed_dict={img: img_pred,
-										K.learning_phase(): 0})
-
-	pass
-
-def load_and_predict():
-	"""	- Loads the model and predicts recurrence behaviour for a 2D list(array) 
-		of images, split into patches and assembles their output into an 2-D for 
-		further processing 
-		- Returns:
-
-	"""
-
-	pass
-
 def E_step(predicted_maps):
 	""" - Applies gaussian smoothing to the predicted probability maps
 		- Generates bit mask for acceptable()discriminative patches for the next M_step
@@ -222,34 +216,52 @@ def E_step(predicted_maps):
 				new_patch_prob = probability*np.gkern()
 				reconstructed_probab_map[center_coord[0]-50: center_coord[0]+50, center_coord[1]-50:center_coord[1]+50] = 
 						np.maximum(orig_patch, new_patch_prob)
-
 			
-			threshold = 
-
-
-
-
-
+			# threshold = 
 
 	pass
 
 
-def main():
-	""" Trains the model on EM algo and saves it """
-	imagewise_patches = load_imagewise_patches() 							# Required in each iteration of EM algo to refine the training data
-	training_data = prepare_train_data_from_img_wise_dict(imagewise_patches)
+#################################################################
+# -------------------- Other Helper functions ------------------#
+#################################################################
+def load_image(img_path):
+	img = cv2.imread(img_path)
+	img_tensor = tf.convert_to_tensor(img)
+	return tf.reshape(img_tensor, [1,101,101,3])					# Reshaping so that it can be processed further into batches on axis=0
 
-	for itr in range(n_iter):
-		
-		# trains and save the model based on the given discriminative patches
-		M_step(training_data, n_epochs, batch_size, ):
+# local response normalization on images (Adapted Code)
+# def lrn(x):
+# 	""" Custom made normalisation layer for Keras """
+#     ones_for_weight = np.reshape(np.ones((32, 32)), (1,32,32))
+#     mu = sum_pool2d(x, pool_size = (7, 7), strides = (1, 1), padding = (3, 3))
+#     mu_weight = sum_pool2d(ones_for_weight, pool_size = (7, 7), strides = (1, 1), padding = (3, 3))
+#     sum_sq_x = sum_pool2d(K.square(x), pool_size = (7, 7), strides = (1, 1), padding = (3, 3))
+#     total_mu_sq = mu_weight * K.square(mu)
+#     sq_cross_term = -2 * K.square(mu)
+#     sigma = K.sqrt(sum_sq_x + total_mu_sq + sq_cross_term)
+#     return (x - mu)/(sigma + 1)
 
-		# Predicts the probab maps and then performs the e-step
-		predicted_probability_maps = load_and_predict(imagewise_patches)
+# # def lcn_output_shape(input_shape):
+# #     return input_shape
 
-		training_data = E_step(predicted_probability_maps)
 
-		# Check when to stop @todo
+# def sum_pool2d(x, pool_size = (7, 7), strides = (1, 1), padding = (3, 3)):
+#     sum_x = pool.pool_2d(x, ds = pool_size, st = strides, mode = 'sum', padding = padding, ignore_border = True)
+#     return sum_x
+
+# # def sum_pool2d_output_shape(input_shape):
+# #     return input_shape
+
+def gkern(kernlen=101, nsig=3):
+    """Returns a 2D Gaussian kernel array."""
+
+    interval = (2*nsig+1.)/(kernlen)
+    x = np.linspace(-nsig-interval/2., nsig+interval/2., kernlen+1)
+    kern1d = np.diff(st.norm.cdf(x))
+    kernel_raw = np.sqrt(np.outer(kern1d, kern1d))
+    kernel = kernel_raw/kernel_raw.sum()
+    return kernel
 
 if __name__ == '__main__':
 	main()
